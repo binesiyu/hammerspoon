eb4a2f3cea5fc4e01b9fdb522808e23960cf5ea2
yubin <1960028060@qq.com>
2017-06-01 09:52:09 +0800

    +apprules

---
+++ appwindow/apprule.lua
@@ -0,0 +1,15 @@
+local E = require('hs.application.watcher')   -- appwindows events
+local A = require('appwindow/appactions')               -- appwindows actions
+------------------
+--  appwindows  --
+------------------
+-- Each app name points to a list of rules, which are event/action pairs.
+-- See hs.application.watcher for events, and appactions.lua for actions.
+local apprules = {
+  rules = {
+    Finder              = {{evt = E.activated,    act = A.toFront}},
+    ['Google Chrome']   = {{evt = E.launched,     act = A.maximize}},
+  },
+}
+
+return apprules
