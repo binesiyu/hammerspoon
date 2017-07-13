f4db1be3147e75ad42be74e34f4c62b8c1ce6b45
yubin <1960028060@qq.com>
2017-07-13 08:59:25 +0800

    switch_window_inapp

--- app.lua
+++ app.lua
@@ -10,15 +10,54 @@ local    applist = {
         {shortcut = 't',appname = '钉钉'},
     }
 
+function switch_window_inapp(app)
+    local focused = app:focusedWindow()
+    if focused then
+      local windows = nil
+      windows = app:allWindows()
+      if #windows <= 1 then
+        return
+      end
+      
+      local to_focus
+      
+      windows = hs.fnutils.filter(windows, hs.window.isStandard)
+      windows = hs.fnutils.filter(windows, hs.window.isVisible)
+
+      -- we need to sort the table just because it's sorted randomly every time
+      table.sort(windows, function(w1, w2)
+        return w1:id() > w2:id()
+      end)
+      local size = #windows
+      -- if no -> no action will be perfomed
+      if size > 1 then
+        local last_index = hs.fnutils.indexOf(windows, focused)
+        if last_index then
+          local focus_index = 1
+          if last_index < size then
+            focus_index = last_index + 1
+          end
+          to_focus = windows[focus_index]
+        end
+      end
+      if to_focus then
+        to_focus:focus()
+      end
+    end
+end
+  
 for i = 1, #applist do
     hs.hotkey.bind('alt', applist[i].shortcut,  function()
         local appname = applist[i].appname
         local appruning = hs.application.get(appname)
         if appruning then
+            --已经是最前面的，切后到同一个程序的不同窗口
             if appruning:isFrontmost() then
               --appruning:hide()
+              switch_window_inapp(appruning)
             elseif appruning:isHidden() then
               appruning:unhide()
+              --激活应用的窗口
             else
               appruning:activate()
             end
